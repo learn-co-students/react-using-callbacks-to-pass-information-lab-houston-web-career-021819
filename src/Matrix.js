6ae26340fe8e86455d4d28f04e093306069d4fb2
EliLauff <eli.lauffenburger@gmail.com>
2019-04-25 15:10:22 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,36 +1,45 @@
-import React, { Component } from 'react';
-import chromeBoi from './data.js'
-import Cell from './Cell.js'
-import ColorSelector from './ColorSelector.js'
+import React, { Component } from "react";
+import chromeBoi from "./data.js";
+import Cell from "./Cell.js";
+import ColorSelector from "./ColorSelector.js";
 
 export default class Matrix extends Component {
+  state = { currentColor: "" };
 
   constructor() {
-    super()
+    super();
   }
 
-  genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
-  )
+  changeColor = color => {
+    this.setState({ currentColor: color });
+  };
 
-  genMatrix = () => (
-    this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
-  )
+  selectedColor = () => {
+    return this.state.currentColor;
+  };
 
+  genRow = vals =>
+    vals.map((val, idx) => (
+      <Cell key={idx} color={val} selectedColor={this.selectedColor} />
+    ));
+
+  genMatrix = () =>
+    this.props.values.map((rowVals, idx) => (
+      <div key={idx} className="row">
+        {this.genRow(rowVals)}
+      </div>
+    ));
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
-        <div id="matrix">
-          {this.genMatrix()}
-        </div>
+        <ColorSelector changeColor={this.changeColor} />
+        <div id="matrix">{this.genMatrix()}</div>
       </div>
-    )
+    );
   }
-
 }
 
 Matrix.defaultProps = {
   values: chromeBoi
-}
+};
